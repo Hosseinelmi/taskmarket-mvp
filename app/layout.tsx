301f89cbae5cac69dd394a9ed78f983156f7ef7c
Hosseinelmi <141170083+Hosseinelmi@users.noreply.github.com>
2026-09-16 18:30:14 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "TaskMarket",
+  description: "هر کاری داری، فقط بگو.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
